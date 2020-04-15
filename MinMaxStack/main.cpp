95aeab9cdd2dddd14c1a0e2590ac076100f07c7a
Adeeb Mohammed <adeeb2358@gmail.com>
2020-04-15 09:37:46 +0900

    min max stack

--- MinMaxStack/main.cpp
+++ MinMaxStack/main.cpp
@@ -7,9 +7,50 @@
 //
 
 #include <iostream>
+#include <vector>
+#include<unordered_map>
+#include <string>
+
+class MinMaxStack {
+private:
+    
+    std::vector<int> stack = {};
+    std::vector<std::unordered_map<std::string, int>> minMaxStack = {};
+    
+public:
+  int peek() {
+    return stack[stack.size()-1];
+  }
+
+  int pop() {
+      minMaxStack.pop_back();
+      int result = stack[stack.size()-1];
+      stack.pop_back();
+      return result;
+  }
+
+  void push(int number) {
+      std::unordered_map<std::string,int> newMinMax = {{"min",number},{"max",number}};
+      if(minMaxStack.size()){
+          std::unordered_map<std::string, int> lastMinMax = minMaxStack[minMaxStack.size() - 1];
+          newMinMax["min"] = std::min(lastMinMax["min"],number);
+          newMinMax["max"] = std::max(lastMinMax["max"],number);
+      }
+      minMaxStack.push_back(newMinMax);
+      stack.push_back(number);
+  }
+
+  int getMin() {
+    return minMaxStack[minMaxStack.size()-1]["min"];
+  }
+
+  int getMax() {
+    return minMaxStack[minMaxStack.size()-1]["max"];
+  }
+    
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout<<"The program for min max stack o(1) time and o(1) space";
     return 0;
 }
